60f8814eeafe1ee20cd57b639ad310d1a5e29368
Ashish <adepuashishkumar565@gmail.com>
2021-08-10 08:55:42 +0530

    after review update

--- src/main/java/com/revature/web/TransactionServlet.java
+++ src/main/java/com/revature/web/TransactionServlet.java
@@ -1,6 +1,7 @@
 package com.revature.web;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,14 +9,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.revature.entity.Transaction;
+import com.revature.repository.TransactionRepository;
+import com.revature.repository.TransactionRepositoryImpl;
 import com.revature.service.TransferService;
 import com.revature.service.TransferServiceImpl;
 
 @SuppressWarnings("serial")
-@WebServlet(urlPatterns = { "/new-transaction" })
+@WebServlet(urlPatterns = { "/new-transaction", "/all-txns" })
 public class TransactionServlet extends HttpServlet{
 
+	private TransactionRepository transactionRepository = new TransactionRepositoryImpl();
 	TransferService transferService = new TransferServiceImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		List<Transaction> txns = transactionRepository.getTransaction(HistoryServlet.account);
+		req.setAttribute("txns", txns);
+		req.getRequestDispatcher("all.jsp").forward(req, resp);
+		
+	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
